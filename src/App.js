d388b956cec961dc799a17ecbf6756544bbcf1f2
Sam <super8989@gmail.com>
2019-10-21 19:13:41 -0400

    starting color box maker - created Box component to render on App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Box from "./Box";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Box height={10} width={20} color='purple' />
+			<Box height={20} width={10} color='teal' />
+		</div>
+	);
 }
 
 export default App;
